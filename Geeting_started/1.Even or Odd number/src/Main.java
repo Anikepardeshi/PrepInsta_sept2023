a194f517eecf70c51f555a978087d45ef78ef4e0
Aniket Pardeshi <aniketpardeshi2001@gmail.com>
2023-11-24 12:49:49 +0530

    stfu

--- Geeting_started/1.Even or Odd number/src/Main.java
+++ Geeting_started/1.Even or Odd number/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		if (number % 2 == 0) {
 			System.out.println(number + " is Even");
 		} else {
-			System.out.println(number + " is odd");
+			System.out.println(number + " is Odd");
 		}
 	}
 }
