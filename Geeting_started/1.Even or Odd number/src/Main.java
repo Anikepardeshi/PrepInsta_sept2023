287027ffc2818a0fb2cd78a64800f41da76f800e
Aniket Pardeshi <aniketpardeshi2001@gmail.com>
2023-11-24 13:07:03 +0530

    hello

--- Geeting_started/1.Even or Odd number/src/Main.java
+++ Geeting_started/1.Even or Odd number/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 		//int number = 29;
 		int number = 28;
 		if (number % 2 == 0) {
-			System.out.println(number + " is Even");
+			System.out.println(number + " is evEn");
 		} else {
 			System.out.println(number + " is Odd");
 		}
